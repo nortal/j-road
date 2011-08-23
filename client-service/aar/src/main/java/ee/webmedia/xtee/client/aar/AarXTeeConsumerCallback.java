cb88a984eb275cdca902a57a7ebf4eecea066f5d
Roman Tekhov <roman.tekhov@gmail.com>
2011-08-23 10:25:28 +0000

    Deleted empty module lkf3
    git-svn-id: http://j-road.googlecode.com/svn/trunk@14 2cd86727-befe-766a-a149-ba6de9b8a225

--- client-service/aar/src/main/java/ee/webmedia/xtee/client/aar/AarXTeeConsumerCallback.java
+++ client-service/aar/src/main/java/ee/webmedia/xtee/client/aar/AarXTeeConsumerCallback.java
@@ -1,18 +1,15 @@
 package ee.webmedia.xtee.client.aar;
 
+import ee.webmedia.xtee.client.service.callback.CustomCallback;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.xml.soap.SOAPBody;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.transform.TransformerException;
-
 import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
-
-import ee.webmedia.xtee.client.service.callback.CustomCallback;
 
 /**
  * @author lauril
@@ -20,13 +17,13 @@ import ee.webmedia.xtee.client.service.callback.CustomCallback;
  */
 @SuppressWarnings("rawtypes")
 public class AarXTeeConsumerCallback extends CustomCallback {
-	
+
 	private Map<String, Attribute> eleAttrs;
-	
+
 	public AarXTeeConsumerCallback() {
 		eleAttrs = new HashMap<String, Attribute>();
 	}
-	
+
 	private void addType(SOAPElement ele) {
 		if (eleAttrs.containsKey(ele.getElementName().getLocalName())) {
 			Attribute attr = eleAttrs.get(ele.getElementName().getLocalName());
@@ -40,21 +37,21 @@ public class AarXTeeConsumerCallback extends CustomCallback {
 			}
 		}
 	}
-	
+
 	public void addElementAttribute(String eleName, String attrName, String attrValue) {
 		eleAttrs.put(eleName, new Attribute(attrName, attrValue));
 	}
-	
+
 	@Override
 	public void doWithMessage(WebServiceMessage request) throws IOException, TransformerException {
 		callback.doWithMessage(request);
-		
+
 		SaajSoapMessage message = (SaajSoapMessage) request;
 		SOAPMessage mes = message.getSaajMessage();
-		
+
 		try {
 			SOAPBody body = mes.getSOAPBody();
-			
+
 			SOAPElement queryEle = (SOAPElement) body.getChildElements().next();
 			SOAPElement kehaEle = (SOAPElement) queryEle.getChildElements().next();
 
@@ -63,17 +60,17 @@ public class AarXTeeConsumerCallback extends CustomCallback {
 				Object nextEle = kehaChilds.next();
 				if (nextEle instanceof SOAPElement) {
 					addType((SOAPElement) nextEle);
-				}				
+				}
 			}
 		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	private class Attribute {
 		private String attrName;
 		private String attrValue;
-		
+
 		public Attribute(String attrName, String attrValue) {
 			this.attrName = attrName;
 			this.attrValue = attrValue;
