b9e0efede48051cf9807c5417afc3947acf3d79d
Roman Tekhov <roman.tekhov@gmail.com>
2011-08-23 15:23:10 +0000

    Updated VANGIS client service
    git-svn-id: http://j-road.googlecode.com/svn/trunk@26 2cd86727-befe-766a-a149-ba6de9b8a225

--- client-service/vangis/src/main/java/ee/webmedia/xtee/client/vangis/VangisXTeeServiceImpl.java
+++ client-service/vangis/src/main/java/ee/webmedia/xtee/client/vangis/VangisXTeeServiceImpl.java
@@ -1,11 +1,8 @@
 package ee.webmedia.xtee.client.vangis;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import ee.webmedia.xtee.client.exception.XTeeServiceConsumptionException;
 import ee.webmedia.xtee.client.service.XTeeDatabaseService;
-import ee.webmedia.xtee.client.vangis.VangisXTeeService;
+import ee.webmedia.xtee.client.vangis.database.VangisXTeeDatabase;
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.KaristuseKohtParing;
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.KaristuseKohtVastus;
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.Oigusnorm;
@@ -18,48 +15,47 @@ import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.produc
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.Paring8Paring;
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.Paring8Vastus;
 import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.PerLiik;
-import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.impl.KaristuseKohtVastusImpl;
-import ee.webmedia.xtee.client.vangis.types.ee.riik.xtee.vangis.producers.producer.vangis.impl.PerLiikImpl;
-import ee.webmedia.xtee.model.XTeeMessage;
-import ee.webmedia.xtee.model.XmlBeansXTeeMessage;
+import java.util.Calendar;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
 
-
+@Service("vangisXTeeService")
 public class VangisXTeeServiceImpl extends XTeeDatabaseService implements VangisXTeeService {
-	
-	
-	
+
+	@Resource
+	private VangisXTeeDatabase vangisXTeeDatabase;
+
+
 	public KaristuseKohtVastus karistuse_koht(String isikukood, String eesnimi, String perenimi, Calendar synniKp) throws XTeeServiceConsumptionException{
 		KaristuseKohtParing paring = KaristuseKohtParing.Factory.newInstance();
 		paring.setIsikukood(isikukood);
 		paring.setEesnimi(eesnimi);
 		paring.setPerekonnanimi(perenimi);
 		paring.setSynnikpv(synniKp);
-		XTeeMessage<KaristuseKohtVastus> vastus = send(new XmlBeansXTeeMessage<KaristuseKohtParing>(paring), "karistuse_koht", "v1");
-		return vastus.getContent();
+
+		return vangisXTeeDatabase.karistuseKohtV1(paring);
 	}
-	
-	
-	public Paring2Vastus paring2(String isikukood, String eesnimi, String perenimi, Calendar synniKp) 
+
+
+	public Paring2Vastus paring2(String isikukood, String eesnimi, String perenimi, Calendar synniKp)
 		throws XTeeServiceConsumptionException{
 		Paring2Paring paring = Paring2Paring.Factory.newInstance();
 		paring.setIsikukood(isikukood);
 		paring.setEesnimi(eesnimi);
 		paring.setPerekonnanimi(perenimi);
 		paring.setSynnikpv(synniKp);
-		XTeeMessage<Paring2Vastus> vastus = send(new XmlBeansXTeeMessage<Paring2Paring>(paring), "paring2" , "v1");
-		return vastus.getContent();
+		return vangisXTeeDatabase.paring2V1(paring);
 	}
-	
+
 	public Paring16Vastus paring16(String isikukood, String eesnimi, String perenimi) throws XTeeServiceConsumptionException{
 		Paring14Paring paring = Paring14Paring.Factory.newInstance();
 		paring.setIsikukood(isikukood);
 		paring.setEesnimi(eesnimi);
 		paring.setPerekonnanimi(perenimi);
-		XTeeMessage<Paring16Vastus> vastus = send(new XmlBeansXTeeMessage<Paring14Paring>(paring), "paring16" , "v1");
-		return vastus.getContent();
+		return vangisXTeeDatabase.paring16V1(paring);
 	}
-	
-	public Paring8Vastus paring8(String perioodiLiik, Calendar algus, Calendar lopp, 
+
+	public Paring8Vastus paring8(String perioodiLiik, Calendar algus, Calendar lopp,
 			String oigusnorm, String parag, String loige, String punkt) throws XTeeServiceConsumptionException{
 		Paring8Paring paring = Paring8Paring.Factory.newInstance();
 		paring.setPerLiik(PerLiik.Enum.forString(perioodiLiik));
@@ -69,17 +65,20 @@ public class VangisXTeeServiceImpl extends XTeeDatabaseService implements Vangis
 		paring.setParagrahv(parag);
 		paring.setLoige(loige);
 		paring.setPunkt(punkt);
-		XTeeMessage<Paring8Vastus> vastus = send(new XmlBeansXTeeMessage<Paring8Paring>(paring), "paring8" , "v1");
-		return vastus.getContent();
+		return vangisXTeeDatabase.paring8V1(paring);
 	}
-	
+
 	public Paring5Vastus paring5(Calendar algus, Calendar lopp, String kood) throws XTeeServiceConsumptionException{
 		Paring5Paring paring = Paring5Paring.Factory.newInstance();
 		paring.setAlgus(algus);
 		paring.setLopp(lopp);
 		paring.setKood(kood);
-		XTeeMessage<Paring5Vastus> vastus = send(new XmlBeansXTeeMessage<Paring5Paring>(paring), "paring5" , "v1");
-		return vastus.getContent();
+		return vangisXTeeDatabase.paring5V1(paring);
 	}
+
+
+  public void setVangisXTeeDatabase(VangisXTeeDatabase vangisXTeeDatabase) {
+    this.vangisXTeeDatabase = vangisXTeeDatabase;
+  }
 
 }
