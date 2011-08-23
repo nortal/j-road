90bc005b57411494872fd470f1be59d7793cc031
Roman Tekhov <roman.tekhov@gmail.com>
2011-08-23 13:28:05 +0000

    Updated E-PRIA client service
    git-svn-id: http://j-road.googlecode.com/svn/trunk@18 2cd86727-befe-766a-a149-ba6de9b8a225

--- client-service/epria/src/main/java/ee/webmedia/xtee/client/tosjuht/EpriaXTeeServiceImpl.java
+++ client-service/epria/src/main/java/ee/webmedia/xtee/client/tosjuht/EpriaXTeeServiceImpl.java
@@ -1,11 +1,8 @@
 package ee.webmedia.xtee.client.tosjuht;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import javax.activation.DataHandler;
-
+import ee.webmedia.xtee.client.exception.XTeeServiceConsumptionException;
+import ee.webmedia.xtee.client.service.XTeeDatabaseService;
+import ee.webmedia.xtee.client.tosjuht.database.EpriaXTeeDatabase;
 import ee.webmedia.xtee.client.tosjuht.model.ManusModel;
 import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.producer.epria.DhsSaadaTaotlusRequest;
 import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.producer.epria.DhsSaadaTaotluseDigiDokRequest;
@@ -17,49 +14,52 @@ import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.produc
 import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.producer.epria.DhsVaataTaotlusePdfResponse;
 import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.producer.epria.Manus;
 import ee.webmedia.xtee.client.tosjuht.types.ee.riik.xtee.epria.producers.producer.epria.VastuseKood;
-import ee.webmedia.xtee.client.exception.XTeeServiceConsumptionException;
-import ee.webmedia.xtee.client.service.XTeeDatabaseService;
 import ee.webmedia.xtee.model.XTeeAttachment;
 import ee.webmedia.xtee.model.XTeeMessage;
 import ee.webmedia.xtee.model.XmlBeansXTeeMessage;
+import java.util.List;
+import java.util.UUID;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Lauri Lättemäe (lauri.lattemaw@webmedia.ee)
  * @date 10.09.2010
  */
+@Service("epriaXTeeService")
 public class EpriaXTeeServiceImpl extends XTeeDatabaseService implements EpriaXTeeService {
+
+  @Resource
+  private EpriaXTeeDatabase epriaXTeeDatabase;
+
   @Override
   public VastuseKood saadaTaotluseDigiDok(String kandeNumber, ManusModel digiDok) throws XTeeServiceConsumptionException {
     DhsSaadaTaotluseDigiDokRequest request = DhsSaadaTaotluseDigiDokRequest.Factory.newInstance();
     request.setKandeNumber(kandeNumber);
-    XTeeMessage<VastuseKood> response = send(new XmlBeansXTeeMessage<DhsSaadaTaotluseDigiDokRequest>(request), "dhsSaadaTaotluseDigiDok");
-    return response.getContent();
+    return epriaXTeeDatabase.dhsSaadaTaotluseDigiDokV1(request);
   }
-  
+
   @Override
   public DhsVaataTaotluseDigiDokResponse vaataTaotluseDigiDok(String kandeNumber) throws XTeeServiceConsumptionException {
     DhsVaataTaotluseDigiDokRequest request = DhsVaataTaotluseDigiDokRequest.Factory.newInstance();
     request.setKandeNumber(kandeNumber);
-    XTeeMessage<DhsVaataTaotluseDigiDokResponse> response = send(new XmlBeansXTeeMessage<DhsVaataTaotluseDigiDokRequest>(request), "dhsVaataTaotluseDigiDok");
-    return response.getContent();
+    return epriaXTeeDatabase.dhsVaataTaotluseDigiDokV1(request);
   }
-  
+
   @Override
   public DhsVaataTaotlusePdfResponse vaataTaotlusePdf(String kandeNumber) throws XTeeServiceConsumptionException {
-	DhsVaataTaotlusePdfRequest request = DhsVaataTaotlusePdfRequest.Factory.newInstance();
+    DhsVaataTaotlusePdfRequest request = DhsVaataTaotlusePdfRequest.Factory.newInstance();
     request.setKandeNumber(kandeNumber);
-    XTeeMessage<DhsVaataTaotlusePdfResponse> response = send(new XmlBeansXTeeMessage<DhsVaataTaotlusePdfRequest>(request), "dhsVaataTaotlusePdf");
-    return response.getContent();
+    return epriaXTeeDatabase.dhsVaataTaotlusePdfV1(request);
   }
 
   @Override
   public DhsVaataTaotluseManusResponse vaataTatoluseManus(Long portaaliId) throws XTeeServiceConsumptionException {
     DhsVaataTaotluseManusRequest request = DhsVaataTaotluseManusRequest.Factory.newInstance();
     request.setManuseId(String.valueOf(portaaliId));
-    XTeeMessage<DhsVaataTaotluseManusResponse> response = send(new XmlBeansXTeeMessage<DhsVaataTaotluseManusRequest>(request), "dhsVaataTaotluseManus");
-    return response.getContent();
+    return epriaXTeeDatabase.dhsVaataTaotluseManusV1(request);
   }
-  
+
   @Override
   public VastuseKood saadaTaotlus(String kandeNumber, ManusModel pdf, List<ManusModel> manused) throws XTeeServiceConsumptionException {
     XTeeMessage<DhsSaadaTaotlusRequest> request = new XmlBeansXTeeMessage<DhsSaadaTaotlusRequest>(DhsSaadaTaotlusRequest.Factory.newInstance());
@@ -70,7 +70,7 @@ public class EpriaXTeeServiceImpl extends XTeeDatabaseService implements EpriaXT
     pdfManus.setManuseId(pdf.getPortaaliId());
     pdfManus.setManuseNimi(pdf.getNimi());
     pdfManus.addNewSisu().setAttachment("cid:" + pdfAttach.getCid());
-    
+
     for (ManusModel manus : manused) {
         XTeeAttachment attach = new XTeeAttachment(manus.getPortaaliId(), manus.getSisu());
         request.getAttachments().add(attach);
@@ -81,5 +81,10 @@ public class EpriaXTeeServiceImpl extends XTeeDatabaseService implements EpriaXT
     }
     XTeeMessage<VastuseKood> response = send(request, "dhsSaadaTaotlus");
     return response.getContent();
+  }
+
+
+  public void setEpriaXTeeDatabase(EpriaXTeeDatabase epriaXTeeDatabase) {
+    this.epriaXTeeDatabase = epriaXTeeDatabase;
   }
 }
