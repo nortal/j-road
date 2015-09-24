2a592a50780312bc80066e164132385788360e53
rometpiho <rometpiho@gmail.com>
2015-09-24 12:59:12 +0300

    digilugu

--- client-service/digilugu/src/main/java/com/nortal/jroad/client/digilugu/DigiluguXTeeService.java
+++ client-service/digilugu/src/main/java/com/nortal/jroad/client/digilugu/DigiluguXTeeService.java
@@ -1,8 +1,8 @@
 package com.nortal.jroad.client.digilugu;
 
-import com.nortal.jroad.client.digilugu.types.hl7BkPqOorgV3.RCMRIN000031UV01Document;
-import com.nortal.jroad.client.digilugu.types.hl7RGHzkorgV3.RCMRIN000029UV01Document;
-import com.nortal.jroad.client.digilugu.types.hl7UEiDvorgV3.RCMRIN000032UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7WErUTorgV3.RCMRIN000032UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000029UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000031UV01Document;
 import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000030UV01Document;
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
 
--- client-service/digilugu/src/main/java/com/nortal/jroad/client/digilugu/DigiluguXTeeServiceImpl.java
+++ client-service/digilugu/src/main/java/com/nortal/jroad/client/digilugu/DigiluguXTeeServiceImpl.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 
 import com.nortal.jroad.client.digilugu.types.ee.riik.xtee.digilugu.producers.producer.digilugu.Hl7Paring;
 import com.nortal.jroad.client.digilugu.types.ee.riik.xtee.digilugu.producers.producer.digilugu.Hl7Vastus;
-import com.nortal.jroad.client.digilugu.types.hl7BkPqOorgV3.RCMRIN000031UV01Document;
-import com.nortal.jroad.client.digilugu.types.hl7RGHzkorgV3.RCMRIN000029UV01Document;
-import com.nortal.jroad.client.digilugu.types.hl7UEiDvorgV3.RCMRIN000032UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7WErUTorgV3.RCMRIN000032UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000029UV01Document;
 import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000030UV01Document;
+import com.nortal.jroad.client.digilugu.types.hl7_orgV3.RCMRIN000031UV01Document;
+import com.nortal.jroad.client.exception.NonTechnicalFaultException;
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
 import com.nortal.jroad.client.service.XTeeDatabaseService;
 import com.nortal.jroad.model.XTeeMessage;
@@ -24,6 +25,7 @@ public class DigiluguXTeeServiceImpl extends XTeeDatabaseService implements Digi
 
   private static final String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
   private static final String HL7 = "hl7";
+  private static final String V1 = "v1";
 
   private final boolean useTestDatabase;
 
@@ -47,25 +49,31 @@ public class DigiluguXTeeServiceImpl extends XTeeDatabaseService implements Digi
   public RCMRIN000030UV01Document getHl7Document(RCMRIN000029UV01Document input) throws XTeeServiceConsumptionException {
     XTeeMessage<Hl7Vastus> vastus = send(input.xmlText());
 
-    RCMRIN000030UV01Document  document = null;
+    RCMRIN000030UV01Document document = null;
     try {
-        document = RCMRIN000030UV01Document.Factory.parse(vastus.getContent().getHl7OutputMessage());
+      document = RCMRIN000030UV01Document.Factory.parse(vastus.getContent().getHl7OutputMessage());
     } catch (XmlException e) {
-        e.printStackTrace();
+      throw new XTeeServiceConsumptionException(new NonTechnicalFaultException(null, "Error parsing xml"),
+                                                getDatabase(),
+                                                HL7,
+                                                V1);
     }
     return document;
 
   }
-  
+
   @Override
   public RCMRIN000032UV01Document getHl7TSK(RCMRIN000031UV01Document input) throws XTeeServiceConsumptionException {
     XTeeMessage<Hl7Vastus> vastus = send(input.xmlText());
 
     RCMRIN000032UV01Document document = null;
     try {
-        document = RCMRIN000032UV01Document.Factory.parse(vastus.getContent().getHl7OutputMessage());
+      document = RCMRIN000032UV01Document.Factory.parse(vastus.getContent().getHl7OutputMessage());
     } catch (XmlException e) {
-        e.printStackTrace();
+      throw new XTeeServiceConsumptionException(new NonTechnicalFaultException(null, "Error parsing xml"),
+                                                getDatabase(),
+                                                HL7,
+                                                V1);
     }
     return document;
 
@@ -75,7 +83,7 @@ public class DigiluguXTeeServiceImpl extends XTeeDatabaseService implements Digi
     Hl7Paring request = Hl7Paring.Factory.newInstance();
     request.setHl7InputMessage(XML + input);
 
-    XTeeMessage<Hl7Vastus> vastus = send(new XmlBeansXTeeMessage<Hl7Paring>(request), HL7, "v1");
+    XTeeMessage<Hl7Vastus> vastus = send(new XmlBeansXTeeMessage<Hl7Paring>(request), HL7, V1);
     return vastus;
   }
 
