7188a735d05858185c4c6cb8d860fa4ad7934f93
Marti Laast <Marti.Laast@nortal.com>
2016-03-18 11:55:22 +0200

    kvkr

---
+++ client-service/kvkr/src/main/java/com/nortal/jroad/client/kvkr/KvkrXTeeServiceImpl.java
@@ -0,0 +1,24 @@
+package com.nortal.jroad.client.kvkr;
+
+import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
+import com.nortal.jroad.client.kvkr.types.ee.x_road.kvkr.producer.ServiceinfoDocument.Serviceinfo;
+import com.nortal.jroad.client.kvkr.types.ee.x_road.kvkr.producer.ServiceinfoResponseDocument.ServiceinfoResponse;
+import com.nortal.jroad.client.service.v2.XTeeDatabaseService;
+import com.nortal.jroad.model.XTeeMessage;
+import com.nortal.jroad.model.XmlBeansXTeeMessage;
+import org.springframework.stereotype.Service;
+
+@Service("kvkrXTeeService")
+public class KvkrXTeeServiceImpl extends XTeeDatabaseService implements KvkrXTeeService {
+
+  public ServiceinfoResponse getServiceinfoV1(String nationalIdCode, String queryGrounds) throws XTeeServiceConsumptionException {
+    Serviceinfo serviceinfo = Serviceinfo.Factory.newInstance();
+    Serviceinfo.Request request = serviceinfo.addNewRequest();
+    request.setNationalId(nationalIdCode);
+    request.setQueryGrounds(queryGrounds);
+
+    XTeeMessage<ServiceinfoResponse> response = send(new XmlBeansXTeeMessage<Serviceinfo>(serviceinfo), "serviceinfo", "v1");
+    return response.getContent();
+  }
+
+}
