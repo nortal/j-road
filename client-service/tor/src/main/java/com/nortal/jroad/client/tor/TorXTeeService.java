1ced25126d5a8e39e168ede9fb15afb3da131d53
rometpiho <rometpiho@gmail.com>
2016-03-24 11:44:00 +0200

    tor

--- client-service/tor/src/main/java/com/nortal/jroad/client/tor/TorXTeeService.java
+++ client-service/tor/src/main/java/com/nortal/jroad/client/tor/TorXTeeService.java
@@ -2,8 +2,14 @@ package com.nortal.jroad.client.tor;
 
 import java.util.Date;
 
+import javax.activation.DataHandler;
+
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
+import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.TORIKDocument;
+import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.DownloadMimeResponseDocument.DownloadMimeResponse;
 import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.TORIKResponseDocument.TORIKResponse;
+import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.UploadMimeResponseDocument.UploadMimeResponse;
+import com.nortal.jroad.model.XTeeMessage;
 /**
  * <code>TOR</code> database X-tee service<br>
  * 
@@ -12,9 +18,22 @@ import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.TORIKResponse
 public interface TorXTeeService {
 
   /**
-   * <code>tor.TORIK</code> service.
+   * <code>emtav5.TORIK</code> service.
    */
   TORIKResponse findTorik(String paringuLiik, Date tootAlgusKp, Date tootLoppKp, String isikukood)
       throws XTeeServiceConsumptionException;
+  
+  /**
+   * <code>emtav5.downloadMime</code> service.
+   */
+  XTeeMessage<DownloadMimeResponse> downloadMime(String target) throws XTeeServiceConsumptionException;
+  
+  /**
+   * <code>emtav5.uploadMime</code> service.
+   */
+  UploadMimeResponse uploadMime(String target, String operation, String id, DataHandler fail)
+      throws XTeeServiceConsumptionException;
+  
+  TORIKDocument getTorikRequest(String paringuLiik, Date tootAlgusKp, Date tootLoppKp, String isikukood);
 
 }
