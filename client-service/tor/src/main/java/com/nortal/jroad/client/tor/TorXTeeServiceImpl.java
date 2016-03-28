2ab238bdc18458b09c80f074028526baa84cd008
rometpiho <rometpiho@gmail.com>
2016-03-28 11:34:59 +0300

    tor

--- client-service/tor/src/main/java/com/nortal/jroad/client/tor/TorXTeeServiceImpl.java
+++ client-service/tor/src/main/java/com/nortal/jroad/client/tor/TorXTeeServiceImpl.java
@@ -19,8 +19,8 @@ import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
 
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
-import com.nortal.jroad.client.service.XRoadDatabaseService;
 import com.nortal.jroad.client.service.callback.CustomCallback;
+import com.nortal.jroad.client.service.v3.XRoadDatabaseService;
 import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.DownloadMimeDocument;
 import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.DownloadMimeDocument.DownloadMime;
 import com.nortal.jroad.client.tor.types.ee.x_road.emtav5.producer.DownloadMimeResponseDocument.DownloadMimeResponse;
