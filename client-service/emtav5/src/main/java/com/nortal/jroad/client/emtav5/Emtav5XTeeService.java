ced6be9c488020bc8c650d4d88511079fa5e4f8f
rometpiho <rometpiho@gmail.com>
2016-04-18 09:14:47 +0300

    emtav5

--- client-service/emtav5/src/main/java/com/nortal/jroad/client/emtav5/Emtav5XTeeService.java
+++ client-service/emtav5/src/main/java/com/nortal/jroad/client/emtav5/Emtav5XTeeService.java
@@ -3,9 +3,15 @@ package com.nortal.jroad.client.emtav5;
 import java.util.Date;
 import java.util.List;
 
+import javax.activation.DataHandler;
+
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.DownloadMimeResponseDocument.DownloadMimeResponse;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SkaMitteresidentResponseType;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SmMaksustatavadAndmedResponseType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckResponseType.Response.Period;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeResponseDocument.UploadMimeResponse;
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
+import com.nortal.jroad.model.XTeeMessage;
 
 /**
  * <code>emtav5</code> (Maksu- ja Tolliamet) database X-tee v5 service.
@@ -15,13 +21,29 @@ import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
 public interface Emtav5XTeeService {
 
 	/**
-	 * <code>emta.xteeFIEAK.v1</code> service.
+	 * <code>emtav5.xteeFIEAK.v1</code> service.
 	 */
 	List<Period> xteeFIEAKV1(String id, Date start, Date end) throws XTeeServiceConsumptionException;
 	
 	/**
-	 * <code>emta.skaMitteresident.v1</code> service.
+	 * <code>emtav5.skaMitteresident.v1</code> service.
 	 */
 	SkaMitteresidentResponseType skaMitteresident(String registreerimiskood) throws XTeeServiceConsumptionException;
 
+	/**
+   * <code>emtav5.smMaksustatavadAndmed.v1</code> service.
+   */
+	SmMaksustatavadAndmedResponseType smMaksustatavadAndmed(String isikukood, String perioodiAlgus, String perioodiLopp)
+      throws XTeeServiceConsumptionException;
+	
+	/**
+   * <code>emtav5.downloadMime</code> service.
+   */
+  XTeeMessage<DownloadMimeResponse> downloadMime(String target) throws XTeeServiceConsumptionException;
+  
+  /**
+   * <code>emtav5.uploadMime</code> service.
+   */
+  UploadMimeResponse uploadMime(String target, String id, DataHandler fail)
+      throws XTeeServiceConsumptionException;
 }
--- client-service/emtav5/src/main/java/com/nortal/jroad/client/emtav5/Emtav5XTeeServiceImpl.java
+++ client-service/emtav5/src/main/java/com/nortal/jroad/client/emtav5/Emtav5XTeeServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.activation.DataHandler;
 import javax.annotation.Resource;
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPElement;
@@ -19,20 +20,36 @@ import org.springframework.ws.WebServiceMessage;
 import org.springframework.ws.soap.saaj.SaajSoapMessage;
 
 import com.nortal.jroad.client.emtav5.database.Emtav5XRoadDatabase;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.DownloadMimeDocument;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.DownloadMimeDocument.DownloadMime;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.DownloadMimeResponseDocument.DownloadMimeResponse;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.DownloadMimeType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SkaMitteresidentDocument.SkaMitteresident;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SkaMitteresidentRequestType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SkaMitteresidentResponseDocument.SkaMitteresidentResponse;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SkaMitteresidentResponseType;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SmMaksustatavadAndmedDocument.SmMaksustatavadAndmed;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SmMaksustatavadAndmedResponseDocument.SmMaksustatavadAndmedResponse;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SmMaksustatavadAndmedResponseType;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SmMaksustatavadAndmedType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckCommonRequestType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckRequestType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckResponseType;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckResponseType.Response;
 import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.SpouseCheckResponseType.Response.Period;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeDocument;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeDocument.UploadMime;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeResponseDocument.UploadMimeResponse;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeType;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeType.Props;
+import com.nortal.jroad.client.emtav5.types.net.x_rd.ee.emtav5.producer.UploadMimeType.Props.Prop;
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
 import com.nortal.jroad.client.service.v3.XRoadDatabaseService;
 import com.nortal.jroad.client.service.callback.CustomCallback;
+import com.nortal.jroad.model.XTeeAttachment;
 import com.nortal.jroad.model.XTeeMessage;
 import com.nortal.jroad.model.XmlBeansXTeeMessage;
+import com.nortal.jroad.util.AttachmentUtil;
 
 /**
  * @author Kait Kasak (kait.kasak@nortal.com)
@@ -42,6 +59,11 @@ public class Emtav5XTeeServiceImpl extends XRoadDatabaseService implements Emtav
 
   private static final String XTEE_FIEAK = "xteeFIEAK";
   private static final String SKA_MITTERESIDENT = "skaMitteresident";
+  private static final String SM_MAKSUSTATAVAD_ANDMED = "smMaksustatavadAndmed";
+  private static final String UPLOAD_ID = "UPLOAD_ID";
+  private static final String UPLOAD_MIME = "uploadMime";
+  private static final String DOWNLOAD_MIME = "downloadMime";
+  private static final String V1 = "v1";
 
   @Resource
   private Emtav5XRoadDatabase emtav5XRoadDatabase;
@@ -55,7 +77,7 @@ public class Emtav5XTeeServiceImpl extends XRoadDatabaseService implements Emtav
 
     XTeeMessage<SpouseCheckResponseType> xteeFIEAKV1 = send(new XmlBeansXTeeMessage<SpouseCheckRequestType>(request),
                                                             XTEE_FIEAK,
-                                                            "v1");
+                                                            V1);
 
     Response response = xteeFIEAKV1.getContent().getResponse();
     return response.getPeriodList();
@@ -67,7 +89,6 @@ public class Emtav5XTeeServiceImpl extends XRoadDatabaseService implements Emtav
     return calendar;
   }
 
-  @Override
   public SkaMitteresidentResponseType skaMitteresident(String registreerimiskood)
       throws XTeeServiceConsumptionException {
     SkaMitteresident input = SkaMitteresident.Factory.newInstance();
@@ -76,13 +97,71 @@ public class Emtav5XTeeServiceImpl extends XRoadDatabaseService implements Emtav
 
     XTeeMessage<SkaMitteresidentResponse> skaMitteresident = send(new XmlBeansXTeeMessage<SkaMitteresident>(input),
                                                                   SKA_MITTERESIDENT,
-                                                                  "v1",
-                                                                  new SkaMitteresidentCallback(),
+                                                                  V1,
+                                                                  new Emtav5Callback(),
                                                                   null);
     return skaMitteresident.getContent().getResponse();
   }
 
-  private static class SkaMitteresidentCallback extends CustomCallback {
+  @Override
+  public SmMaksustatavadAndmedResponseType smMaksustatavadAndmed(String isikukood,
+                                                                 String perioodiAlgus,
+                                                                 String perioodiLopp)
+                                                                     throws XTeeServiceConsumptionException {
+    SmMaksustatavadAndmed input = SmMaksustatavadAndmed.Factory.newInstance();
+    SmMaksustatavadAndmedType request = input.addNewRequest();
+    request.setIsikukood(isikukood);
+    request.setPerioodiAlgus(perioodiAlgus);
+    request.setPerioodiLopp(perioodiLopp);
+
+    XTeeMessage<SmMaksustatavadAndmedResponse> skaMitteresident = send(new XmlBeansXTeeMessage<SmMaksustatavadAndmed>(input),
+                                                                       SM_MAKSUSTATAVAD_ANDMED,
+                                                                       V1,
+                                                                       new Emtav5Callback(),
+                                                                       null);
+    return skaMitteresident.getContent().getResponse();
+  }
+
+  @Override
+  public XTeeMessage<DownloadMimeResponse> downloadMime(String target) throws XTeeServiceConsumptionException {
+    DownloadMime downloadMimeDocument = DownloadMimeDocument.DownloadMime.Factory.newInstance();
+
+    DownloadMimeType request = downloadMimeDocument.addNewRequest();
+    request.setTarget(target);
+
+    XTeeMessage<DownloadMimeResponse> response = send(new XmlBeansXTeeMessage<DownloadMimeDocument.DownloadMime>(downloadMimeDocument),
+                                                      DOWNLOAD_MIME,
+                                                      V1,
+                                                      new Emtav5Callback(),
+                                                      null);
+    return response;
+  }
+
+  @Override
+  public UploadMimeResponse uploadMime(String target, String id, DataHandler fail)
+      throws XTeeServiceConsumptionException {
+    UploadMime uploadMimeDocument = UploadMimeDocument.UploadMime.Factory.newInstance();
+
+    UploadMimeType request = uploadMimeDocument.addNewRequest();
+    request.setTarget(target);
+    Props props = request.addNewProps();
+    Prop prop = props.addNewProp();
+    prop.setKey(UPLOAD_ID);
+    prop.setStringValue(id);
+
+    XmlBeansXTeeMessage<UploadMimeDocument.UploadMime> xteeMessage = new XmlBeansXTeeMessage<UploadMimeDocument.UploadMime>(uploadMimeDocument);
+    List<XTeeAttachment> attachments = xteeMessage.getAttachments();
+
+    String failCid = AttachmentUtil.getUniqueCid();
+    request.addNewFile().setHref("cid:" + failCid);
+    attachments.add(new XTeeAttachment(failCid, fail));
+
+    XTeeMessage<UploadMimeResponse> response = send(xteeMessage, UPLOAD_MIME, V1, new Emtav5Callback(), null);
+
+    return response.getContent();
+  }
+
+  private static class Emtav5Callback extends CustomCallback {
     public void doWithMessage(WebServiceMessage message) throws IOException, TransformerException {
       callback.doWithMessage(message);
       try {
@@ -103,7 +182,7 @@ public class Emtav5XTeeServiceImpl extends XRoadDatabaseService implements Emtav
         Iterator headers = env.getHeader().getChildElements();
         while (headers.hasNext()) {
           SOAPElement header = (SOAPElement) headers.next();
-           if (header.getNamespaceURI().equalsIgnoreCase("http://x-rd.net/xsd/xroad.xsd")) {
+          if (header.getNamespaceURI().equalsIgnoreCase("http://x-rd.net/xsd/xroad.xsd")) {
             String localHeaderName = header.getLocalName();
             QName qName = new QName("http://x-road.ee/xsd/x-road.xsd", localHeaderName, "xro");
             header.setElementQName(qName);
