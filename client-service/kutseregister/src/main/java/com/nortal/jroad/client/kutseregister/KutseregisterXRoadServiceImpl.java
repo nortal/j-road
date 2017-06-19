4eca8308724a955617b29ea9a3d81e67524344e0
Hasso Mehide <hasso.mehide@nortal.com>
2017-06-19 10:04:55 +0300

    Kutseregister: add missing namespaces to response

--- client-service/kutseregister/src/main/java/com/nortal/jroad/client/kutseregister/KutseregisterXRoadServiceImpl.java
+++ client-service/kutseregister/src/main/java/com/nortal/jroad/client/kutseregister/KutseregisterXRoadServiceImpl.java
@@ -4,15 +4,20 @@ import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
 import com.nortal.jroad.client.kutseregister.database.KutseregisterXRoadDatabase;
 import com.nortal.jroad.client.kutseregister.types.ee.riik.xtee.kutseregister.producers.producer.kutseregister.KutsetunnistusDocument.Kutsetunnistus;
 import com.nortal.jroad.client.kutseregister.types.ee.riik.xtee.kutseregister.producers.producer.kutseregister.KutsetunnistusVastusDocument.KutsetunnistusVastus;
+import org.apache.commons.lang.StringUtils;
+import org.apache.xmlbeans.XmlCursor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.xml.namespace.QName;
 
 /**
  * @author Anti Orgla
  */
 @Service("kutseregisterXRoadServiceImpl")
 public class KutseregisterXRoadServiceImpl implements KutseregisterXRoadService {
+
+  private static final String KUTSEREGISTER_NS = "http://producers.kutseregister.xtee.riik.ee/producer/kutseregister";
 
   @Resource
   private KutseregisterXRoadDatabase kutseregisterXRoadDatabase;
@@ -26,7 +31,23 @@ public class KutseregisterXRoadServiceImpl implements KutseregisterXRoadService 
     if (nimi != null) {
       kutsetunnistus.setNimi(nimi);
     }
-    return kutseregisterXRoadDatabase.kutsetunnistusV2(kutsetunnistus);
+    KutsetunnistusVastus response = kutseregisterXRoadDatabase.kutsetunnistusV2(kutsetunnistus);
+    addMissingNamespaces(response);
+    return response;
+  }
+
+  private void addMissingNamespaces(KutsetunnistusVastus response) {
+    XmlCursor cursor = response.newCursor();
+    while (cursor.hasNextToken()) {
+      cursor.toNextToken();
+      if (cursor.isStart()) {
+        QName name = cursor.getName();
+        if (StringUtils.isBlank(name.getNamespaceURI())) {
+          cursor.setName(new QName(KUTSEREGISTER_NS, name.getLocalPart()));
+        }
+      }
+    }
+    cursor.dispose();
   }
 
 }
