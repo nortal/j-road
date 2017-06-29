4de47d7fa93e44c0ea8b457777bc1b809cd78367
Priit Liivak <priitliivak@gmail.com>
2017-06-29 23:21:33 +0300

    Update publish.sh
    quieting one more script

--- deploy/publish.sh
+++ deploy/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn deploy --settings $DEPLOY_DIR/settings.xml -DperformRelease=true -DskipTests=true
+    mvn deploy --settings $DEPLOY_DIR/settings.xml -DperformRelease=true -DskipTests=true -q
     exit $?
 fi
