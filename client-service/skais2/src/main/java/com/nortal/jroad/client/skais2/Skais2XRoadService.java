e9e93b471fb6edc3e49aafbc31b3a218fd8464b3
annika <annika800@gmail.com>
2018-01-30 14:04:01 +0200

    SKAIS2 uued teenused

--- client-service/skais2/src/main/java/com/nortal/jroad/client/skais2/Skais2XRoadService.java
+++ client-service/skais2/src/main/java/com/nortal/jroad/client/skais2/Skais2XRoadService.java
@@ -1,10 +1,14 @@
 package com.nortal.jroad.client.skais2;
 
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
-import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusRequest;
-import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusResponse;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusRequest;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusResponse;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusVastusResponse;
 import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedRequest;
 import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedResponse;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusRequest;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusResponse;
+import com.nortal.jroad.model.XTeeMessage;
 
 /**
  * @author Hasso Mehide <hasso.mehide@nortal.com>
@@ -20,4 +24,15 @@ public interface Skais2XRoadService {
      * Skais2.TKToovoimPuueHyvitised.v1
      */
 	TKToovoimPuueHyvitisedResponse tkToovoimPuueHyvitisedV1(TKToovoimPuueHyvitisedRequest request) throws XTeeServiceConsumptionException;
+
+  /**
+   * Skais2.TKToovoimPuueHyvitisedMassTeenus.v1
+   */
+  TKToovoimPuueHyvitisedMassTeenusResponse submitTKToovoimPuueHyvitisedMassTeenusV1(TKToovoimPuueHyvitisedMassTeenusRequest request) throws XTeeServiceConsumptionException;
+
+  /**
+   * Skais2.TKToovoimPuueHyvitisedMassTeenusVastus.v1
+   */
+  XTeeMessage<TKToovoimPuueHyvitisedMassTeenusVastusResponse> getTKToovoimPuueHyvitisedMassTeenusVastusV1(long protsessiId) throws XTeeServiceConsumptionException;
+
 }
--- client-service/skais2/src/main/java/com/nortal/jroad/client/skais2/Skais2XRoadServiceImpl.java
+++ client-service/skais2/src/main/java/com/nortal/jroad/client/skais2/Skais2XRoadServiceImpl.java
@@ -1,32 +1,60 @@
 package com.nortal.jroad.client.skais2;
 
 import com.nortal.jroad.client.exception.XTeeServiceConsumptionException;
-import com.nortal.jroad.client.skais2.database.Skais2XRoadDatabase;
-import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusRequest;
-import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusResponse;
+import com.nortal.jroad.client.service.v4.XRoadDatabaseService;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusRequest;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusResponse;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusVastusRequest;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedMassTeenusVastusResponse;
 import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedRequest;
 import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TKToovoimPuueHyvitisedResponse;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusRequest;
+import com.nortal.jroad.client.skais2.types.ee.som.skais2.producer.skais2.TVHYhisTaotlusResponse;
+import com.nortal.jroad.model.XTeeMessage;
+import com.nortal.jroad.model.XmlBeansXTeeMessage;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 /**
  * @author Hasso Mehide <hasso.mehide@nortal.com>
  */
 @Service("skais2XRoadService")
-public class Skais2XRoadServiceImpl implements Skais2XRoadService {
-
-    @Resource
-    private Skais2XRoadDatabase skais2XRoadDatabase;
+public class Skais2XRoadServiceImpl extends XRoadDatabaseService implements Skais2XRoadService {
 
     @Override
     public TVHYhisTaotlusResponse tvhTaotlusYksUksV1(TVHYhisTaotlusRequest request) throws XTeeServiceConsumptionException {
-        return skais2XRoadDatabase.tvhTaotlusYksUksV1(request);
+        XTeeMessage<TVHYhisTaotlusResponse> response = send(new XmlBeansXTeeMessage<TVHYhisTaotlusRequest>(request), "TVHTaotlusYksUks", "v1");
+        return response.getContent();
     }
 	
     @Override
     public TKToovoimPuueHyvitisedResponse tkToovoimPuueHyvitisedV1(TKToovoimPuueHyvitisedRequest request) throws XTeeServiceConsumptionException {
-        return skais2XRoadDatabase.tkToovoimPuueHyvitisedV1(request);
+        XTeeMessage<TKToovoimPuueHyvitisedResponse> response =
+                send(new XmlBeansXTeeMessage<TKToovoimPuueHyvitisedRequest>(request), "TKToovoimPuueHyvitised", "v1");
+        return response.getContent();
+    }
+
+
+    @Override
+    public TKToovoimPuueHyvitisedMassTeenusResponse submitTKToovoimPuueHyvitisedMassTeenusV1(TKToovoimPuueHyvitisedMassTeenusRequest request)
+            throws XTeeServiceConsumptionException {
+        XTeeMessage<TKToovoimPuueHyvitisedMassTeenusResponse> response =
+                send(new XmlBeansXTeeMessage<TKToovoimPuueHyvitisedMassTeenusRequest>(request), "TKToovoimPuueHyvitisedMassTeenus", "v1");
+
+        return response.getContent();
+    }
+
+    @Override
+    public XTeeMessage<TKToovoimPuueHyvitisedMassTeenusVastusResponse> getTKToovoimPuueHyvitisedMassTeenusVastusV1(long protsessiId)
+            throws XTeeServiceConsumptionException {
+        TKToovoimPuueHyvitisedMassTeenusVastusRequest request = TKToovoimPuueHyvitisedMassTeenusVastusRequest.Factory.newInstance();
+        request.setProtsessID(protsessiId);
+
+        XTeeMessage<TKToovoimPuueHyvitisedMassTeenusVastusResponse> response =
+                send(new XmlBeansXTeeMessage<TKToovoimPuueHyvitisedMassTeenusVastusRequest>(request),
+                        "TKToovoimPuueHyvitisedMassTeenusVastus",
+                        "v1");
+
+        return response;
     }
 
 }
