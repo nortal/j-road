8d89972a77194657f8a684d17aae0b2cafc58a3d
Artur Aleksander Kanošin <arturaleksander.kanosin@nortal.com>
2025-11-14 10:36:36 +0200

    EMPIS2-1743: restore old implementation

--- server/src/main/java/com/nortal/jroad/wsdl/XTeePortTypesProvider.java
+++ server/src/main/java/com/nortal/jroad/wsdl/XTeePortTypesProvider.java
@@ -2,10 +2,12 @@ package com.nortal.jroad.wsdl;
 
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Pattern;
 
-import javax.naming.NameNotFoundException;
 import javax.wsdl.Definition;
+import javax.wsdl.Message;
 import javax.wsdl.Operation;
 import javax.wsdl.PortType;
 import javax.wsdl.WSDLException;
@@ -14,7 +16,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import com.nortal.jroad.annotation.XTeeService;
-import org.springframework.ws.server.endpoint.MessageEndpoint;
+import com.nortal.jroad.endpoint.AbstractXTeeBaseEndpoint;
 import org.springframework.ws.wsdl.wsdl11.provider.SuffixBasedPortTypesProvider;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -22,64 +24,110 @@ import org.w3c.dom.Element;
 import com.ibm.wsdl.Constants;
 import com.nortal.jroad.mapping.XTeeEndpointMapping;
 
+import static java.util.function.Predicate.not;
+
 /**
  * Part of the serverside WSDL generator
- *
- * @author Dmitri Danilkin
- * @author Lauri Lättemäe (lauri.lattemae@nortal.com) - protocol 4.0
  */
 public class XTeePortTypesProvider extends SuffixBasedPortTypesProvider {
+  private static final Pattern DOT = Pattern.compile("\\.");
+
   private XTeeEndpointMapping xRoadEndpointMapping;
+
+  public void setXRoadEndpointMapping(XTeeEndpointMapping xRoadEndpointMapping) {
+    this.xRoadEndpointMapping = xRoadEndpointMapping;
+  }
 
   @Override
   @SuppressWarnings("unchecked")
   public void addPortTypes(Definition definition) throws WSDLException {
     super.addPortTypes(definition);
-    Document doc = null;
-    try {
-      doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
-    } catch (ParserConfigurationException e) {
-      throw new RuntimeException(e);
-    }
-
+    Document doc = createDocument();
     for (Entry<QName, PortType> entry : (Set<Entry<QName, PortType>>) definition.getPortTypes().entrySet()) {
       for (Operation operation : (List<Operation>) entry.getValue().getOperations()) {
         Element docElement = doc.createElementNS(Constants.NS_URI_WSDL, "documentation");
         docElement.setPrefix("wsdl");
+
         Element titleEelement = doc.createElementNS(XTeeWsdlDefinition.XROAD_NAMESPACE, "title");
         titleEelement.setPrefix(XTeeWsdlDefinition.XROAD_PREFIX);
-
-        try {
-          // Get endpoint
-          MessageEndpoint endpoint = null;
-          for (String method : xRoadEndpointMapping.getMethods()) {
-            String methodTail = method.substring(method.indexOf('.') + 1).toLowerCase();
-            if (methodTail.startsWith(operation.getName().toLowerCase() + ".")) {
-              endpoint = xRoadEndpointMapping.getMethodMap().get(method);
-              break;
-            }
-          }
-          if (endpoint == null)
-            throw new NameNotFoundException();
-
-          // Get annotation from endpoint and check that it contains a name.
-          XTeeService xTeeServiceAnnotation = endpoint.getClass().getAnnotation(XTeeService.class);
-          if (xTeeServiceAnnotation == null || xTeeServiceAnnotation.title().isEmpty())
-            throw new NameNotFoundException();
-
-          titleEelement.appendChild(doc.createTextNode(xTeeServiceAnnotation.title()));
-        } catch (NameNotFoundException e) {
-          titleEelement.appendChild(doc.createTextNode(operation.getName()));
-        }
+        String title = getTitle(operation);
+        titleEelement.appendChild(doc.createTextNode(title));
 
         docElement.appendChild(titleEelement);
+
         operation.setDocumentationElement(docElement);
       }
     }
 
   }
 
-  public void setXRoadEndpointMapping(XTeeEndpointMapping xRoadEndpointMapping) {
-    this.xRoadEndpointMapping = xRoadEndpointMapping;
+  private String getTitle(Operation operation) {
+    return getMatchingEndpoint(operation)
+               .map(Object::getClass)
+               .map(clazz -> clazz.getAnnotation(XTeeService.class))
+               .map(XTeeService::title)
+               .filter(not(String::isEmpty))
+               .orElse(operation.getName());
+  }
+
+  private Optional<AbstractXTeeBaseEndpoint> getMatchingEndpoint(Operation operation) {
+    String operationServiceCode = operation.getName().toLowerCase() + ".";
+    return xRoadEndpointMapping.getMethodMap()
+               .entrySet()
+               .stream()
+               .filter(methodEndpoint -> operationServiceCode.equals(normalizedServiceCode(methodEndpoint.getKey())))
+               .map(Entry::getValue)
+               .findAny();
+  }
+
+  private static String normalizedServiceCode(String methodKey) {
+    String[] tokens = DOT.split(methodKey);
+    if (tokens.length == 3) {
+      return tokens[1].toLowerCase();
+    }
+    throw new IllegalStateException("Malformed method key in endpointMapping: " + methodKey);
+  }
+
+  private static Document createDocument() {
+    try {
+      return DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+    } catch (ParserConfigurationException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  protected String getOperationName(Message message) {
+    String messageName = this.getMessageName(message);
+    if (messageName == null) {
+      return null;
+    }
+
+    if (messageName.endsWith(this.getRequestSuffix())) {
+      return messageName.substring(0, messageName.length() - this.getRequestSuffix().length());
+    }
+
+    if (messageName.endsWith(this.getFaultSuffix())) {
+      return messageName.substring(0, messageName.length() - this.getFaultSuffix().length());
+    }
+
+    if (messageName.endsWith(this.getResponseSuffix())) {
+      return messageName.substring(0, messageName.length() - this.getResponseSuffix().length());
+    }
+
+    return messageName;
+  }
+
+  @Override
+  protected boolean isInputMessage(Message message) {
+    String messageName = this.getMessageName(message);
+    return messageName != null
+           && !messageName.endsWith(this.getFaultSuffix())
+           && !messageName.endsWith(this.getResponseSuffix());
+  }
+
+  // from org.springframework.ws.wsdl.wsdl11.provider.SuffixBasedPortTypesProvider.getMessageName()
+  private String getMessageName(Message message) {
+    return message.getQName().getLocalPart();
   }
 }
